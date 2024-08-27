b08f0338472f03d986c467b7263c33df6864d31a
sarangof <sarangofc@gmail.com>
2024-08-27 12:30:38 -0500

    Re-structuring of the OSMnx command, more outputs are being gathered. Roads are polished before return.

--- gather_data_pilot.py
+++ gather_data_pilot.py
@@ -49,34 +49,50 @@ def overturemaps_save(overture_file, request_type: str, id: int):
         print(f"Skipping save for Overture ID: {id}")
     return None
 
+
+def remove_duplicate_roads(osm_roads):
+    osm_roads_reset = osm_roads.reset_index()
+    osm_roads_reset['sorted_pair'] = osm_roads_reset.apply(lambda row: tuple(sorted([row['u'], row['v']])), axis=1)
+    osm_roads = osm_roads_reset.drop_duplicates(subset=['sorted_pair'])
+    osm_roads = osm_roads.drop(columns=['sorted_pair'])
+    return osm_roads
+
+
 #@delayed
 def osmnx_command(bbox):
     print(f"Running OSM command with bbox={bbox}")
     try:
-        oms_buildings = ox.features_from_bbox(bbox=bbox, tags={'building': True})
-        oms_buildings = oms_buildings.apply(lambda c: c.astype(str) if c.name != "geometry" else c, axis=0)
+        osm_buildings = ox.features_from_bbox(bbox=bbox, tags={'building': True})
+        osm_buildings = osm_buildings.apply(lambda c: c.astype(str) if c.name != "geometry" else c, axis=0)
 
         G = ox.graph_from_bbox(bbox=bbox, network_type='drive')
-        intersections, roads = ox.graph_to_gdfs(G)
+        osm_intersections, osm_roads = ox.graph_to_gdfs(G)
 
-        return oms_buildings, oms_roads, oms_intersections
+        return osm_buildings, osm_intersections, osm_roads
     except Exception as e:
         print(f"Error getting OSM data: {e}")
         return None
 
 #@delayed
 def osmnx_save(osm_files, id: int):
-    if osm_file is not None:
-        output_path = f"./output_data/OSM_building_footprints_highways_{id}.gpkg"
-        print(f"Saving OSM file to {output_path}")
-        try:
-            osm_file.to_file(output_path, driver="GPKG")
-            return output_path
-        except Exception as e:
-            print(f"Error saving OSM file: {e}")
+    if osm_files is not None:
+        for file_type, file in osm_files.items():
+            try:
+                output_path = f"./output_data/{file_type}_{id}.gpkg"
+                print(f"Saving OSM file to {output_path}")
+                if file_type =='OSM_roads':
+                    file = remove_duplicate_roads(file)
+                    file.drop(columns=['osmid','reversed']).to_file(output_path, driver="GPKG")    
+                else:
+                    file.to_file(output_path, driver="GPKG")    
+            except Exception as e:
+                print(f"Error saving OSM file: {e}")
+        return True
     else:
         print(f"Skipping save for OSM ID: {id}")
     return None
+
+
 
 def make_requests(partition):
     print(f"Processing partition with {len(partition)} rows")
@@ -103,10 +119,14 @@ def make_requests(partition):
             # Open street maps commands
             bbox = [rectangle['maxy'], rectangle['miny'], rectangle['maxx'], rectangle['minx']]
             print("About to trigger osm command")
-            oms_file = osmnx_command(bbox)
-            if oms_file is not None:
+            osm_buildings, osm_intersections, osm_roads = osmnx_command(bbox)
+            osm_files = {'OSM_buildings': osm_buildings, 
+                         'OSM_intersections': osm_intersections, 
+                         'OSM_roads': osm_roads}
+            # NEED TO MAKE SURE THIS COMMAND REALLY WORKS.
+            if osm_files is not None: 
                 print("About to trigger osm save")
-                save_result = osmnx_save(oms_file, index + 1)
+                save_result = osmnx_save(osm_files, index + 1)
                 results.append(save_result)
         except Exception as e:
             print(f"OSM error: {e}")
@@ -118,8 +138,8 @@ def run_all():
     rectangles = gpd.read_file('data/rectangles.geojson')
     
     # Print columns and first few rows
-    print(f"Columns in the rectangles DataFrame: {rectangles.columns.tolist()}")
-    print(rectangles.head())
+    #print(f"Columns in the rectangles DataFrame: {rectangles.columns.tolist()}")
+    #print(rectangles.head())
     
     # Prepare DataFrame for Dask
     rectangles[['minx', 'miny', 'maxx', 'maxy']] = rectangles.bounds
@@ -142,3 +162,4 @@ def run_all():
 # Execute the function
 run_all()
 print("All tasks completed.")
+
