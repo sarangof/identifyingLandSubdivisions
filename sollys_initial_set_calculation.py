31d52ea59f920ae8984aa81a6a281e6be27b66e9
sarangof <sarangofc@gmail.com>
2024-10-10 10:57:19 -0500

    working version.

--- sollys_initial_set_calculation.py
+++ sollys_initial_set_calculation.py
@@ -5,13 +5,14 @@ from metric_plots import *
 import fiona
 from pyproj import CRS
 from shapely.geometry import box
+import json
 import cProfile
 import pstats
 from io import StringIO
 
 
 rectangles = gpd.read_file('./data/rectangles.geojson')
-
+blocks_clipped_empty = 0 
 metrics_pilot = []
 row_epsilon = 0.01
 
@@ -23,7 +24,7 @@ for rectangle_id, rectangle in rectangles.iterrows():
     print(f"rectangle_id: {rectangle_id}")
     rectangle_centroid = rectangle.geometry.centroid
     utm_proj_rectangle = get_utm_proj(float(rectangle_centroid.x), float(rectangle_centroid.y))
-    
+
     geometry = gpd.GeoSeries(rectangle['geometry'])
 
     rectangle_projected = gpd.GeoDataFrame({'geometry': geometry}, crs="EPSG:4326").to_crs(epsg=CRS.from_proj4(utm_proj_rectangle).to_epsg())
@@ -77,7 +78,11 @@ for rectangle_id, rectangle in rectangles.iterrows():
 
     bounding_box = rectangle_projected.bounds.values[0]
     bounding_box_geom = box(*bounding_box)
-    buildings_clipped = buildings[buildings.geometry.intersects(bounding_box_geom)]
+    try:
+        buildings_clipped = buildings[buildings.geometry.intersects(bounding_box_geom)]
+        buildings_clipped = buildings_clipped[(buildings_clipped['confidence']>0.75)|buildings_clipped['confidence'].isna()].reset_index()
+    except KeyError:
+        continue
 
     rectangle_area = calculate_area_geodesic(rectangle)
 
@@ -85,13 +90,15 @@ for rectangle_id, rectangle in rectangles.iterrows():
         blocks = get_blocks(road_union, roads)
     else:
         blocks = gpd.GeoDataFrame([])
+
     if not blocks.empty:
-        blocks_clipped = blocks[blocks.geometry.intersects(rectangle_projected['geometry'])]
+        blocks_clipped = blocks[blocks.geometry.intersects(bounding_box_geom)]#blocks[blocks.geometry.intersects(rectangle_projected['geometry'])]
         blocks_clipped_within_rectangle = blocks_clipped.clip(bounding_box_geom)
         area_tiled_by_blocks = blocks_clipped_within_rectangle.area.sum()
         share_tiled_by_blocks = area_tiled_by_blocks/rectangle_area
     else:
         blocks_clipped = gpd.GeoDataFrame([])
+        blocks_clipped_empty += 1
     #blocks_clipped = blocks.clip(list(rectangle_projected.geometry.bounds))
 
 
@@ -121,7 +128,7 @@ for rectangle_id, rectangle in rectangles.iterrows():
     # Metric 6 -- building azimuth
     if not buildings_clipped.empty:
         n_orientation_groups = 4
-        m6, buildings_clipped = metric_6_deviation_of_building_azimuth(buildings_clipped, n_orientation_groups)
+        m6, buildings_clipped = metric_6_deviation_of_building_azimuth(buildings_clipped, n_orientation_groups, rectangle_id)
         #plot_azimuth(buildings_clipped, roads, rectangle_projected, rectangle_id, n_orientation_groups)
     else:
         m6 = np.nan
@@ -138,19 +145,24 @@ for rectangle_id, rectangle in rectangles.iterrows():
         #plot_two_row_blocks(rectangle_id, rectangle_projected, blocks_clipped, internal_buffers, buildings_clipped, roads, row_epsilon=0.1)
         #plot_two_row_blocks(rectangle_id, rectangle_projected, blocks_clipped, internal_buffers, buildings_clipped, roads, row_epsilon=0.001)
     else:
+        print("Blocks_clipped is empty")
         m7, m8 = np.nan, np.nan
 
     
-    if (not roads_clipped.empty) and (not OSM_intersections_clipped.empty):
+    if (not roads_clipped.empty):
         # Metric 9 -- tortuosity index
         rectangle_projected_arg = rectangle_projected.geometry
-        m9, all_road_vertices = metric_9_tortuosity_index(rectangle_id, roads_clipped, OSM_intersections_clipped, rectangle_projected_arg, angular_threshold=30, tortuosity_tolerance=5)
-                                                        
+        #m9, all_road_vertices = metric_9_tortuosity_index(rectangle_id, roads_clipped, OSM_intersections_clipped, rectangle_projected_arg, angular_threshold=30, tortuosity_tolerance=5)
+        m9 = metric_9_tortuosity_index_option_B(roads_clipped)
+    else:
+        m9 = np.nan
+
+    if (not OSM_intersections_clipped.empty) and ((not roads.empty)):                                               
         # Metric 20 -- average angle between road segments
-        m10 = metric_10_average_angle_between_road_segments(OSM_intersections, roads)
+        m10 = metric_10_average_angle_between_road_segments(OSM_intersections_clipped, roads)
         #plot_inflection_points(rectangle_id, rectangle_projected, all_road_vertices, roads)
     else:
-        m9, m10 = np.nan, np.nan
+        m10 = np.nan
 
     if not roads_clipped.empty:
         road_length = roads_clipped.length.sum()
@@ -191,10 +203,31 @@ for rectangle_id, rectangle in rectangles.iterrows():
                         })
 
 metrics_pilot = pd.DataFrame(metrics_pilot)
+final_geo_df = gpd.GeoDataFrame(pd.merge(rectangles, metrics_pilot, how='left', left_on='n', right_on='index'), geometry=rectangles.geometry)
 
-full_index = pd.Index(range(1, 50))
-missing_indices = full_index.difference(metrics_pilot.index)
-missing_df = pd.DataFrame(index=missing_indices, columns=metrics_pilot.columns)
 
-# Concatenate the original DataFrame with the missing rows, and sort by index
-metrics_pilot = pd.concat([metrics_pilot, missing_df]).sort_index()
+all_metrics_columns = ['metric_1','metric_2','metric_3','metric_4','metric_5','metric_6','metric_7','metric_8','metric_9','metric_10']
+final_geo_df[all_metrics_columns]
+metrics_with_magnitude = ['metric_2','metric_3','metric_5','metric_6','metric_7','metric_10']
+#scalar_metrics = ['metric_2','metric_3','metric_5','metric_6','metric_7','metric_10']
+not_inverted_metrics = ['metric_2','metric_6','metric_7','metric_8']
+
+final_geo_df[['metric_2_original', 'metric_3_original', 'metric_5_original', 'metric_6_original', 'metric_7_original', 'metric_10_original']] = final_geo_df[['metric_2', 'metric_3', 'metric_5', 'metric_6', 'metric_7', 'metric_10']].copy()
+
+final_geo_df.loc[:,all_metrics_columns] = (
+    final_geo_df[all_metrics_columns]
+    .apply(lambda x: (x - x.mean()) / (x.std()), axis=0)
+)
+
+final_geo_df.loc[:,all_metrics_columns] = (
+    final_geo_df[all_metrics_columns]
+    .apply(lambda x: (x - x.min()) / (x.max()-x.min()), axis=0)
+)
+
+metrics_to_invert = [col for col in all_metrics_columns if col not in not_inverted_metrics]
+metrics_to_invert_names = [col+'_invert' for col in all_metrics_columns if col not in not_inverted_metrics]
+
+
+final_geo_df[metrics_to_invert_names] = final_geo_df[metrics_to_invert].apply(lambda x: 1-x, axis=0)
+
+final_geo_df['irregularity_index'] = final_geo_df[all_metrics_columns].mean(axis=1)
