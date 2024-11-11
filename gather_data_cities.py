275760a434ce22b7fd92d577e5c922c07ded5d84
sarangof <sarangofc@gmail.com>
2024-11-11 17:06:05 -0500

    Organized and centralized methods with the current file naming conventions.

--- gather_data_cities.py
+++ gather_data_cities.py
@@ -23,10 +23,18 @@ intersections_path = f'{input_path}/intersections'
 urban_extents_path = f'{input_path}/urban_extents'
 output_path = f'{main_path}/output'
 
+city_info_path = f'{input_path}/city_info'
+extents_path = f'{city_info_path}/extents'
+analysis_buffers_path = f'{city_info_path}/analysis_buffers'
+search_buffers_path = f'{city_info_path}/search_buffers'
+grids_path = f'{city_info_path}/grids'
+output_path = f'{main_path}/output'
+
 # List of cities for which to gather data
 cities = ["Belo Horizonte", "Campinas", "Bogota", "Nairobi", "Bamako", 
           "Lagos", "Accra", "Abidjan", "Mogadishu", "Cape Town", 
           "Maputo", "Luanda"]
+cities = [city.replace(' ', '_') for city in cities]
 
 # Useful auxiliary functions
 def remove_duplicate_roads(osm_roads):
@@ -53,61 +61,31 @@ def get_utm_proj(lon, lat):
     is_northern = lat >= 0  # Determine if the zone is in the northern hemisphere
     return f"+proj=utm +zone={utm_zone} +{'north' if is_northern else 'south'} +ellps=WGS84 +datum=WGS84 +units=m +no_defs"
 
-def osm_command(city_name,transformed_road_search_area):
+def osm_command(city_name, search_area):
     custom_filter = '["highway"~"trunk|motorway|primary|secondary|tertiary|primary_link|secondary_link|tertiary_link|trunk_link|motorway_link|residential|unclassified|road"]'
-    G = ox.graph_from_polygon(polygon=transformed_road_search_area, custom_filter=custom_filter)
+    #G = ox.graph_from_polygon(polygon=transformed_road_search_area, custom_filter=custom_filter)
+    if len(search_area) > 0:
+        polygon = search_area.geometry.iloc[0]  # Extract the first geometry
+        G = ox.graph_from_polygon(polygon=polygon, custom_filter=custom_filter)
+    else:
+        print("The search area is empty.")
+    
     osm_intersections, osm_roads = ox.graph_to_gdfs(G)
-        
-    # Ensure the directory exists
-    output_dir = f"{roads_path}/{city_name}"
-    os.makedirs(output_dir, exist_ok=True)
 
     # Process and save roads
     osm_roads = remove_duplicate_roads(osm_roads)
-    road_output_file = f"{output_dir}/{city_name}_OSM_roads.gpkg"
+    output_dir_roads = f"{roads_path}/{city_name}"
+    os.makedirs(output_dir_roads, exist_ok=True)
+    road_output_file = f"{output_dir_roads}/{city_name}_OSM_roads.gpkg"
     osm_roads = osm_roads.drop(columns=['osmid','reversed'])
     osm_roads = remove_list_columns(osm_roads)
     osm_roads.to_file(road_output_file, driver="GPKG")  
 
     # Save intersections file
-    osm_intersections.to_file(f"{output_dir}/{city_name}_OSM_intersections.gpkg", driver="GPKG")
+    output_dir_intersections = f"{intersections_path}/{city_name}"
+    os.makedirs(output_dir_intersections, exist_ok=True)
+    osm_intersections.to_file(f"{output_dir_intersections}/{city_name}_OSM_intersections.gpkg", driver="GPKG")
 
-def overture_maps_command(city_name,transformed_road_search_area):
-    request_type='building'
-    bbox_str = ','.join([str(transformed_road_search_area.bounds[x]) for x in range(4)])
-    print(f"Running Overture command with bbox_str={bbox_str} and request_type={request_type}")
-    command = [
-        "overturemaps", "download",
-        "-f", "geojson",
-        "--bbox=" + bbox_str,
-        "--type=" + request_type
-    ]
-    result = subprocess.run(command, capture_output=True, text=True)
-
-    # Ensure the directory exists
-    output_dir = f"{roads_path}/{city_name}"
-    os.makedirs(output_dir, exist_ok=True)
-
-    if result.returncode == 0:
-        geoparquet_data = result.stdout
-        try:
-            overture_file = gpd.read_file(StringIO(geoparquet_data))
-        except Exception as e:
-            print(f"Error reading Overture data: {e}")
-    else:
-        print(f"Error occurred while running Overture command: {result.stderr}")
-    if overture_file is not None:
-        output_path = f'{output_dir}/Overture_{request_type}_{city_name}.geojson'
-        print(f"Saving Overture file to {output_path}")
-        try:
-            overture_file.to_file(output_path)
-        except Exception as e:
-            print(f"Error saving Overture file: {e}")
-    else:
-        print(f"Skipping save for Overture in city: {city_name}")
-
-# Need to get city urban extent
-# 
 
 #@delayed
 def overturemaps_command(bbox_str, request_type: str):
@@ -129,12 +107,12 @@ def overturemaps_command(bbox_str, request_type: str):
             return None
     else:
         print(f"Error occurred while running Overture command: {result.stderr}")
-        return None
+    return None
 
 #@delayed
-def overturemaps_save(overture_file, request_type: str, id: int):
+def overturemaps_save(overture_file, output_dir, request_type: str, city_name: str):
     if overture_file is not None:
-        output_path = f'./output_data/Overture_{request_type}_{id}.geojson'
+        output_path = f'{output_dir}/Overture_{request_type}_{city_name}.geojson' #f'./output_data/Overture_{request_type}_{id}.geojson'
         print(f"Saving Overture file to {output_path}")
         try:
             overture_file.to_file(output_path)
@@ -148,39 +126,42 @@ def overturemaps_save(overture_file, request_type: str, id: int):
 
 
 def make_requests(partition):
-    print(f"Processing partitioxn with {len(partition)} rows")
+    print(f"Processing partition with {len(partition)} rows")
     results = []
-    for city_name in partition.iterrows():
+    # OJO: THIS NEEDS TO BE CHANGED
+    for city_name in partition.city:
         print(city_name)
 
         # NEED TO GET THE CITY URBAN EXTENT
-
-        # NEED TO GET THE SEARCH AREA
+        #urban_extent = gpd.read_parquet(f'{extents_path}/{city_name}/{city_name}_urban_extent.parquet'
+        search_area = gpd.read_parquet(f'{search_buffers_path}/{city_name}/{city_name}_search_buffer.parquet') 
 
         # Get city geometries in localized UTMs
-        utm_proj_city = get_utm_proj(float(city.geometry.centroid.x), float(city.geometry.centroid.y))
+        utm_proj_city = get_utm_proj(float(search_area.geometry.centroid.x), float(search_area.geometry.centroid.y))
         transformer = pyproj.Transformer.from_crs(pyproj.CRS('EPSG:4326'), utm_proj_city, always_xy=True)
         def transform_geom(geom):
             return transform(transformer.transform, geom)
-        transformed_city_geom = transform_geom(city.geometry)
+        
+        #transformed_sarch_area_geom = transform_geom(search_area.geometry[0]) 
+        #road_search_area = gpd.GeoSeries(transformed_sarch_area_geom,crs=utm_proj_city).geometry[0].buffer(2000)        #transformed_urban_border = transform(pyproj.Transformer.from_crs(utm_proj_city, pyproj.CRS('EPSG:4326'), always_xy=True).transform, urban_border)
+        #transformed_road_search_area = transform(pyproj.Transformer.from_crs(utm_proj_city, pyproj.CRS('EPSG:4326'), always_xy=True).transform, road_search_area)
+        #transformed_road_search_area = transform(pyproj.Transformer.from_crs(utm_proj_city, pyproj.CRS('EPSG:4326'), always_xy=True).transform, transformed_sarch_area_geom)
+        osm_command(city_name,search_area)#transformed_road_search_area)
 
-        custom_filter = '["highway"~"trunk|motorway|primary|secondary|tertiary|primary_link|secondary_link|tertiary_link|trunk_link|motorway_link|residential|unclassified|road"]'
-        road_search_area = gpd.GeoSeries(transformed_city_geom,crs=utm_proj_city).geometry[0].buffer(2000)
-        #transformed_urban_border = transform(pyproj.Transformer.from_crs(utm_proj_city, pyproj.CRS('EPSG:4326'), always_xy=True).transform, urban_border)
-        transformed_road_search_area = transform(pyproj.Transformer.from_crs(utm_proj_city, pyproj.CRS('EPSG:4326'), always_xy=True).transform, road_search_area)
-        osm_command(city_name,transformed_road_search_area)
-
-        # NEED TO GET SEARCH AREA BOUNDARIES
         try:
             # Overture maps commands
-            bbox_str = ','.join(rectangle[['minx', 'miny', 'maxx', 'maxy']].astype(str))
+            search_area_bounds = search_area.bounds
+            bbox_str = ','.join(search_area_bounds[['minx', 'miny', 'maxx', 'maxy']].values[0].astype(str))
             request_type = 'building'
             print("About to trigger overturemaps command")
             overture_file = overturemaps_command(bbox_str, request_type)
             
             if overture_file is not None:
                 print("About to trigger overturemaps save")
-                save_result = overturemaps_save(overture_file,request_type, index + 1)
+                        # Ensure the directory exists
+                output_dir_buildings = f"{buildings_path}/{city_name}"
+                os.makedirs(output_dir_buildings, exist_ok=True)
+                save_result = overturemaps_save(overture_file, output_dir_buildings, request_type, city_name)
                 results.append(save_result)
         except Exception as e:
             print(f"Overture error: {e}")
@@ -190,7 +171,7 @@ def make_requests(partition):
 
 # Paralellize data gathering for all cities in the list.
 def run_all():
-    cities_set = pd.DataFrame(cities)
+    cities_set = pd.DataFrame({'city':cities})
 
     # Create Dask DataFrame
     cities_set_ddf = dd.from_pandas(cities_set, npartitions=4)
---
+++ main.py
@@ -0,0 +1,7 @@
+
+
+# Run EE data fetch
+
+# Run gather data cities
+
+# Run citywide_calculation
