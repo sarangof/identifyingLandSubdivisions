21d566564613a96d3d5f31661992a43265704c8d
sarangof <sarangofc@gmail.com>
2024-11-11 17:51:44 -0500

    Added trivial standardization for similar treatment of all metrics.

--- standardize_metrics.py
+++ standardize_metrics.py
@@ -4,6 +4,9 @@ import numpy as np
 Here, 0 will be irregular and 1 will be subdivision
 """
 
+def standardize_metric_1(series):
+    return series
+
 def standardize_metric_2(series):
     series = np.where(series > 100, 100, series)
     return 1 - (series/100)
@@ -12,9 +15,15 @@ def standardize_metric_3(series):
     series = np.where(series > 40, 40, series)
     return series/40
 
+def standardize_metric_4(series):
+    return series
+
 def standardize_metric_5(series):
     series = np.where(series > 324, 324, series)
     return series/324
+
+def standardize_metric_6(series):
+    return series
 
 def standardize_metric_7(series):
     series = np.where(series < 30, 30, series)
@@ -28,6 +37,9 @@ def standardize_metric_8(series):
     """
     series = np.where(series > 1, 1, series)
     return (1 - series)
+
+def standardize_metric_9(series):
+    return series
 
 def standardize_metric_10(series):
     return 1-(series/90)
@@ -45,11 +57,15 @@ def standardize_metric_13(series):
 
 # Map metrics to their respective functions
 standardization_functions = {
+    'metric_1': standardize_metric_1,
     'metric_2': standardize_metric_2,
     'metric_3': standardize_metric_3,
+    'metric_4': standardize_metric_4,
     'metric_5': standardize_metric_5,
+    'metric_6': standardize_metric_6,
     'metric_7': standardize_metric_7,
     'metric_8': standardize_metric_8,
+    'metric_9': standardize_metric_9,
     'metric_10': standardize_metric_10,
     'metric_11': standardize_metric_11,
     'metric_12': standardize_metric_12,
