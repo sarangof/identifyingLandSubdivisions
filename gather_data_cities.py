e7b9d18347a35b18c442e7096bcafa6fb9a62671
sarangof <sarangofc@gmail.com>
2024-12-12 12:53:17 +0100

    Adapted path naming convention.

--- gather_data_cities.py
+++ gather_data_cities.py
@@ -9,18 +9,18 @@ import pyproj
 from shapely.ops import transform
 
 # Paths Configuration
-main_path = "/Users/sarangof/Documents/Identifying Land Subdivisions/data"
-input_path = os.path.join(main_path, "input")
-buildings_path = os.path.join(input_path, "buildings")
-roads_path = os.path.join(input_path, "roads")
-intersections_path = os.path.join(input_path, "intersections")
-urban_extents_path = os.path.join(input_path, "urban_extents")
-output_path = os.path.join(main_path, "output")
-output_path_csv = os.path.join(output_path, "csv")
-search_buffers_path = os.path.join(input_path, "city_info", "search_buffers")
+MAIN_PATH = "/Users/sarangof/Documents/Identifying Land Subdivisions/data"
+INPUT_PATH = os.path.join(MAIN_PATH, "input")
+BUILDINGS_PATH = os.path.join(INPUT_PATH, "buildings")
+ROADS_PATH = os.path.join(INPUT_PATH, "roads")
+INTERSECTIONS_PATH = os.path.join(INPUT_PATH, "intersections")
+URBAN_EXTENTS_PATH = os.path.join(INPUT_PATH, "urban_extents")
+OUTPUT_PATH = os.path.join(MAIN_PATH, "output")
+OUTPUT_PATH_CSV = os.path.join(OUTPUT_PATH, "csv")
+SEARCH_BUFFER_PATH = os.path.join(MAIN_PATH, "city_info", "search_buffers")
 
 # Ensure paths exist
-os.makedirs(output_path_csv, exist_ok=True)
+os.makedirs(OUTPUT_PATH_CSV, exist_ok=True)
 
 # Utility Functions
 def remove_duplicate_roads(osm_roads):
@@ -51,26 +51,26 @@ def osm_command(city_name, search_area):
 
     # Process and save roads
     osm_roads = remove_duplicate_roads(osm_roads)
-    output_dir_roads = os.path.join(roads_path, city_name)
+    output_dir_roads = os.path.join(ROADS_PATH, city_name)
     os.makedirs(output_dir_roads, exist_ok=True)
     road_output_file = os.path.join(output_dir_roads, f"{city_name}_OSM_roads.gpkg")
     osm_roads = remove_list_columns(osm_roads)
     osm_roads.to_file(road_output_file, driver="GPKG")
 
     # Save intersections
-    output_dir_intersections = os.path.join(intersections_path, city_name)
+    output_dir_intersections = os.path.join(INTERSECTIONS_PATH, city_name)
     os.makedirs(output_dir_intersections, exist_ok=True)
     osm_intersections.to_file(os.path.join(output_dir_intersections, f"{city_name}_OSM_intersections.gpkg"), driver="GPKG")
 
 def overturemaps_download_and_save(bbox_str, request_type: str, output_dir, city_name: str):
     os.makedirs(output_dir, exist_ok=True)
-    output_path = os.path.join(output_dir, f'Overture_{request_type}_{city_name}.geoparquet')
+    OUTPUT_PATH_OVERTURE = os.path.join(output_dir, f'Overture_{request_type}_{city_name}.geoparquet')
     command = [
         "overturemaps", "download",
         "-f", "geoparquet",
         "--bbox=" + bbox_str,
         "--type=" + request_type,
-        "--output=" + output_path
+        "--output=" + OUTPUT_PATH_OVERTURE
     ]
     result = subprocess.run(command, capture_output=True, text=True)
     if result.returncode == 0:
@@ -87,7 +87,7 @@ def make_requests(partition):
     results = []
     for city_name in partition.city:
         try:
-            search_area = gpd.read_parquet(os.path.join(search_buffers_path, city_name, f"{city_name}_search_buffer.geoparquet"))
+            search_area = gpd.read_parquet(os.path.join(SEARCH_BUFFER_PATH, city_name, f"{city_name}_search_buffer.geoparquet"))
             utm_proj_city = get_utm_proj(float(search_area.geometry.centroid.x.iloc[0]), float(search_area.geometry.centroid.y.iloc[0]))
             transformer = pyproj.Transformer.from_crs(pyproj.CRS('EPSG:4326'), utm_proj_city, always_xy=True)
 
@@ -95,7 +95,7 @@ def make_requests(partition):
 
             search_area_bounds = search_area.bounds
             bbox_str = ','.join(search_area_bounds[['minx', 'miny', 'maxx', 'maxy']].values[0].astype(str))
-            results.append(overturemaps_download_and_save(bbox_str, "building", os.path.join(buildings_path, city_name), city_name))
+            results.append(overturemaps_download_and_save(bbox_str, "building", os.path.join(BUILDINGS_PATH, city_name), city_name))
         except Exception as e:
             error_message = str(e).split("\n")[0]  # Extract only the first line of the error
             print(f"Error for {city_name}: {e}")  # Print full error to terminal
@@ -112,7 +112,7 @@ def run_all(cities):
     results_df = results.compute()
 
     # Save logs to a CSV file
-    results_df.to_csv(os.path.join(output_path_csv, "data_gather_logs.csv"), index=False)
+    results_df.to_csv(os.path.join(OUTPUT_PATH_CSV, "data_gather_logs.csv"), index=False)
 
 def main():
     cities = ["Belo Horizonte", "Campinas", "Bogota", "Nairobi", "Bamako", "Lagos", "Accra", "Abidjan", "Cape Town", "Maputo", "Mogadishu", "Luanda"]
