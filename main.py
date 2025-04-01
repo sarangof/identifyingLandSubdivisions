bbc7084e1c185982a13f37b3e19886efa6140329
sarangof <sarangofc@gmail.com>
2025-04-01 17:26:54 -0500

    Moved aux functions.

--- main.py
+++ main.py
@@ -28,38 +28,6 @@ OUTPUT_PATH_RASTER = f'{OUTPUT_PATH}/raster'
 OUTPUT_PATH_PNG = f'{OUTPUT_PATH}/png'
 OUTPUT_PATH_RAW = f'{OUTPUT_PATH}/raw_results'
 
-@delayed
-def get_epsg(city_name):
-    search_buffer = f'{SEARCH_BUFFER_PATH}/{city_name}/{city_name}_search_buffer.geoparquet'
-    extent = dgpd.read_parquet(search_buffer)
-    geometry = extent.geometry[0].compute()
-    epsg = get_utm_crs(geometry)
-    print(f'{city_name} EPSG: {epsg}')
-    return epsg
-
-def load_dataset(path, epsg=None):
-    dataset = dgpd.read_parquet(path, npartitions=4)
-    
-    # Only assign if the file has no CRS
-    if epsg:
-        if dataset.crs is None:
-            dataset = dataset.set_crs("EPSG:4326")  # assume WGS84 if missing
-        dataset = dataset.to_crs(epsg)
-
-    return dataset
-
-@delayed
-def row_count(dgdf):
-    """Count the rows in a dataframe"""
-    row_count = dgdf.map_partitions(len).compute().sum()
-
-    return row_count
-
-
-def test_math(input):
-    return input + input
-
-
 
 @delayed
 def metrics(city_name,YOUR_NAME,grid_size):
